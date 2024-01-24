d2e89796fb20826f4c156c92aff795edcfc54919
Cory404 <coryletombe1@gmail.com>
2024-01-24 14:30:22 +0100

    pas sur la team

---
+++ Services/Membres/Decorator/EtudiantDecorator.php
@@ -0,0 +1,18 @@
+<?php 
+
+class EtudiantDecorator implements MembreInterface {
+    protected $membre;
+
+    public function __construct(MembreInterface $membre) {
+        $this->membre = $membre;
+    }
+
+    public function emprunter() {
+        $this->membre->emprunter();
+        echo "Limite d'emprunt pour les étudiants.\n";
+    }
+
+    public function retourner() {
+        $this->membre->retourner();
+    }
+}
---
+++ Services/Membres/Membre.php
@@ -0,0 +1,9 @@
+<?php
+
+class Membre implements MembreInteface {
+    public function emprunter();
+    echo `Livre emprunté par un membre.\n`
+}
+    public function retourner();{
+    echo `Livre retourné par un membre.\n`
+}
