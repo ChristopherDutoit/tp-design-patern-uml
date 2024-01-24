3db7aaf564a23ecdb9a6c68a52b502b1f753e1c3
christopher <christopher.dutoit2002@gmail.com>
2024-01-24 10:31:44 +0100

    view login signin

---
+++ Views/signin.view.php
@@ -0,0 +1,17 @@
+<h2>Formulaire d'Inscription</h2>
+
+<form action="" method="post">
+    <label for="fname">Prénom:</label><br>
+    <input type="text" id="fname" name="fname" required><br>
+
+    <label for="lname">Nom:</label><br>
+    <input type="text" id="lname" name="lname" required><br>
+
+    <label for="email">E-mail:</label><br>
+    <input type="email" id="email" name="email" required><br>
+    
+    <label for="password">Mot de passe</label><br/>
+    <input type="password" idp="password" name="password" required><br/>
+
+    <input type="submit" value="S'inscrire">
+</form> 
