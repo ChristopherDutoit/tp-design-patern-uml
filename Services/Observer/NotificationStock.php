cba9c255794e0696b8f76dc5c392da9d08bf15ae
Cory404 <coryletombe1@gmail.com>
2024-01-24 13:34:17 +0100

    NotificationAdmin

--- Services/Observer/NotificationStock.php
+++ Services/Observer/NotificationStock.php
@@ -1,13 +1,9 @@
 <?php
 
-class NotificationStockObserver implements ObserverInterface {
+class NotificationStock implements ObserverInterface {
     public function update($livre, $action) {
         echo "Notification Stock : Livre {$action} - Titre : {$livre->getTitle()}, Auteur : {$livre->getAuthor()}\n";
-    }
-}
-
-class NotificationAdminObserver implements ObserverInterface {
-    public function update($livre, $action) {
-        echo "Notification Admin : Livre {$action} - Titre : {$livre->getTitle()}, Auteur : {$livre->getAuthor()}\n";
+        $stockManager = new StockManager();
+        $stockManager->updateStock($livre->getTitle(), $livre->getAuthor(), $action);
     }
 }
